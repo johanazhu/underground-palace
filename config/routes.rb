9037701f8d5a177b836469246b9d11943fc7e3dd
johan <t511069160@qq.com>
2023-03-23 16:43:30 +0800

    通过脚手架生成article的增删改查

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  resources :articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
-  root "rails/welcome#index"
+  root "articles#index"
+  # root "rails/welcome#index"
 end
