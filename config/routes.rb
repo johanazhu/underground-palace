ca6f49c60def143f9cb3020fba474e6a61755bb7
johan <t511069160@qq.com>
2023-03-26 10:02:57 +0800

    完成登录注册页映射

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # 将默认登录路由更改为 /login：
+  devise_scope :user do
+    get '/login' => 'devise/sessions#new'
+    get '/register' => 'devise/registrations#new'
+  end
+
   resources :articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
