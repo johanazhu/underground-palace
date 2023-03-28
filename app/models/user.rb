47f55c8df35f8033311036e9a0fef6d5d2610b84
johan <t511069160@qq.com>
2023-03-28 09:03:47 +0800

    注册页面加上

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # attr_accessible :email, :password, :password_confirmation, :remember_me, :username
+  # attr_accessible :username, :email, :password, :password_confirmation, :remember_me,
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 Rails.application.routes.draw do
 
-  devise_for :users, controllers: {
-    sessions: 'users/sessions'
+  devise_for :users, path: "", path_names: {
+    sign_in: "login",
+    sign_out: "logout",
+    sign_up: "register" 
+  }, controllers: {
+    registrations: "users/registrations",
+    sessions: "users/sessions",
   }
   # 将默认登录路由更改为 /login：
   devise_scope :user do
     get '/login' => 'devise/sessions#new'
     get '/register' => 'devise/registrations#new'
   end
+
+
 
   resources :articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
