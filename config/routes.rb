e19e8367122f5677f1967193979a64909a60b82c
johan <t511069160@qq.com>
2023-03-28 11:29:06 +0800

    设置页基础样式

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,8 @@ Rails.application.routes.draw do
 
 
   resources :articles
+
+  resources :settings, only: [:index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
