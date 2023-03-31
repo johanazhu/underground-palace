1488a3e49e62720a32e643221426e5d52b760007
johan <t511069160@qq.com>
2023-03-31 13:58:51 +0800

    整理profile逻辑

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,36 +1,30 @@
 class ProfilesController < ApplicationController
-    # skip_before_action :authenticate_user, only: [:show], raise: false
-    before_action :authenticate_user!
-    # before_action :set_profile
+    before_action :authenticate_user!, except: [:show]
+    before_action :set_profile
 
     def show
-        @profile = User.find_by_username(params[:username])
     end
 
     def follow
-        p "=================="
-        p params[:article_id]   
-        # @article = Article.find(params[:article_id])
-        @user = User.find_by_username(params[:username])
         current_user.follow @user
 
         respond_to do |format|
             format.js
         end
-
     end
 
     def unfollow
-        @user = User.find_by_username(params[:username])
         current_user.unfollow @user
+
         respond_to do |format|
             format.js
         end
     end
 
-    # private
-    # def set_profile
-    #     @profile = User.find_by_username(params[:username])
-    # end
+    private
+    
+    def set_profile
+        @user = User.find_by_username(params[:username])
+    end
 
 end
