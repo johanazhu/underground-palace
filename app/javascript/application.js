76466ff42ff674377ef721ac69b02a33220b2619
johan <t511069160@qq.com>
2023-04-07 15:17:59 +0800

    订阅和热门

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,3 +4,12 @@ import "./controllers"
 import * as bootstrap from "bootstrap"
 import $ from 'jquery'
 import {} from 'jquery-ujs'
+
+$(function() {
+    $(".nav-link").on('click', function () {
+      console.log('点击')
+      $(".nav-link").removeClass("active");
+      $(this).addClass("active");
+    })
+
+});
